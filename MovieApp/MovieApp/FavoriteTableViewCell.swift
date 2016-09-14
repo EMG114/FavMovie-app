f48b2352d3de66be88eef0343c63253ef5a470a2
EMG114 <egutierrez411@gmail.com>
2016-09-13 22:33:28 -0400

    customized Fav Table VC, created custom cell, no data

---
+++ MovieApp/MovieApp/FavoriteTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteTableViewCell.swift
+//  MovieApp
+//
+//  Created by Erica on 9/13/16.
+//  Copyright © 2016 Erica Gutierrez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+//import Cocoa
+
+class FavoriteTableViewCell: UITableViewCell {
+    
+    
+    
+    
+    @IBOutlet weak var favMoviePosterImage: UIImageView!
+    @IBOutlet weak var favMovieTitleLabel: UILabel!
+    @IBOutlet weak var favMovieYearLabel: UILabel!
+    @IBOutlet weak var favMovieDirectorLabel: UILabel!
+    @IBOutlet weak var favWriterLabel: UILabel!
+    
+    @IBOutlet weak var favActorsLabel: UILabel!
+    
+    
+
+}
--- MovieApp/MovieCollectionCell.swift
+++ MovieApp/MovieCollectionCell.swift
@@ -13,20 +13,6 @@ class MovieCollectionCell: UICollectionViewCell {
     @IBOutlet weak var moviePosterImage: UIImageView!
     @IBOutlet weak var movieTitle: UILabel!
     
-    
-    var movieDirector:String = ""
-    var movieWriter:String = ""
-    var movieActors:String = ""
-    var movieID:String = ""
-    var moviePosterUrl:String = ""
-    var movieRated:String = ""
-    var movieLanguage:String = ""
-    var movieCountry:String = ""
-    var movieGenre:String = ""
-    var movieMetascore: String = ""
-    var movieRating:String = ""
-    var moviePlotShort:String = ""
-    var movieRuntime:String = ""
 
     
    
